85fa6eadf1f6e5b8de21dc0f6c13d27a58e793fc
ssperandio <ssperandio@artit.com.br>
2021-07-21 17:51:07 -0300

    Botão de download funcionando

--- js/script.js
+++ js/script.js
@@ -1,7 +1,25 @@
 $('button').click(function() 
 {
-  var conteudo = $('#conteudoQRCode').val();
+  var conteudo = $('#content').val();
   var GoogleCharts = 'https://chart.googleapis.com/chart?chs=500x500&cht=qr&chl=';
   var imagemQRCode = GoogleCharts + conteudo;
   $('#imageQRCode').attr('src', imagemQRCode);
 });	
+
+let title = document.getElementById('#title');
+let btnDownload = document.querySelector('#download');
+let img = document.querySelector('#imageQRCode');
+
+btnDownload.addEventListener('click', () => {
+  let imgPath = img.getAttribute('src');
+  let fileName = title+'.png';
+
+  saveAs(imgPath, fileName);
+});
+
+function getFileName(str){
+  return str.substring(str.lastIndexOf('/')+1);
+}
+
+/* (caso der problema no download do QR apagar a variável local title
+  e em filename substituir para 'your_qr.png') */
